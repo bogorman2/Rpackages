237df5a1f35c8dade9f06f64f796024d866525f0
bogorman2 <b.ogorman2@universityofgalway.ie>
2025-06-05 12:14:41 +0100

    IrishRegionalWeatherData20142024

---
+++ IrishRegionalWeatherData2024/R/ReadWeatherData.R
@@ -0,0 +1,47 @@
+install.packages("readr")
+install.packages("lubridate")
+install.packages("tidyverse")
+library(readr)
+library(lubridate)
+library(tidyverse)
+
+START_YEAR=2014
+FINISH_YEAR=2024
+
+prepare_2 <- function(file,region,l_skip=17){
+  #for most weather station files the header is 17 lines long
+  f_df <- read_csv(file,skip=l_skip) # reads in csv file skipping the first l_skip lines. This removes the header so that we get a proper dataframe
+  # add a column for wind speed if it is missing
+  if(!("wdsp" %in% names(f_df))){
+    f_df <- mutate (f_df,wdsp=NA)
+  }
+  # add a column for wind direction if it is missing
+  if(!("wddir" %in% names(f_df))){
+    f_df <- mutate (f_df,wddir=NA)
+  }
+  # change date to date field and add year, month,day, hour, and minute columns then select everything
+  # and then filter to only between choosen years
+  f_df %>% select(date,rain,temp,-wetb,-dewpt,-vappr,rhum,msl,wdsp,wddir,-starts_with("ind")) %>%
+    mutate(date=dmy_hm(date),
+           year=year(date),
+           month=month(date),
+           day=day(date),
+           hour=hour(date),
+           region=region)    %>%
+    select(region, year, month, day, hour, everything()) %>%
+    filter(year >= START_YEAR, year <= FINISH_YEAR)
+}
+East_Region_Weather<-prepare_2("data_raw/DublinAirport.csv","East",23)
+saveRDS(East_Region_Weather,"data/East_Region_Weather.rdata")
+
+West_Region_Weather<-prepare_2("data_raw/MaceHead.csv","West",)
+saveRDS(West_Region_Weather,"data/West_Region_Weather.rdata")
+
+Central_Region_Weather<-prepare_2("data_raw/Mullingar.csv","Central")
+saveRDS(Central_Region_Weather,"data/Central_Region_Weather.rdata")
+
+South_Region_Weather<-prepare_2("data_raw/Valentia.csv","South",23)
+saveRDS(South_Region_Weather,"data/South_Region_Weather.rdata")
+
+North_Region_Weather<-prepare_2("data_raw/MalinHead.csv","North",23)
+saveRDS(North_Region_Weather,"data/North_Region_Weather.rdata")
