04a1b000219e0f9ff7dadbaf77c2c3c7d28d271c
bogorman2 <b.ogorman2@universityofgalway.ie>
2025-06-05 12:21:25 +0100

    Met Eireann Acknowledgement

--- IrishRegionalWeatherData2024/R/ReadWeatherData.R
+++ IrishRegionalWeatherData2024/R/ReadWeatherData.R
@@ -5,6 +5,10 @@ library(readr)
 library(lubridate)
 library(tidyverse)
 
+# Met Éireann retains Intellectual Property Rights and copyright over this data.
+# Met Éireann is acknowledged as the source of this data available at Met.ie.
+# Met Éireann does not accept any liability whatsoever for any error or omission in the data series, their availability, or for any loss or damage arising from their use.
+
 START_YEAR=2014
 FINISH_YEAR=2024
 
