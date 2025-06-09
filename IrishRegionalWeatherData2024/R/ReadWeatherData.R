c43b4d36181edbcaabcd31cf8bb0c7289d3a0aa7
bogorman2 <b.ogorman2@universityofgalway.ie>
2025-06-09 12:34:18 +0100

    addingcsvsaving

--- IrishRegionalWeatherData2024/R/ReadWeatherData.R
+++ IrishRegionalWeatherData2024/R/ReadWeatherData.R
@@ -9,7 +9,7 @@ library(tidyverse)
 # Met Éireann is acknowledged as the source of this data available at Met.ie.
 # Met Éireann does not accept any liability whatsoever for any error or omission in the data series, their availability, or for any loss or damage arising from their use.
 
-START_YEAR=2014
+START_YEAR=2015
 FINISH_YEAR=2024
 
 prepare_2 <- function(file,region,l_skip=17){
@@ -35,17 +35,25 @@ prepare_2 <- function(file,region,l_skip=17){
     select(region, year, month, day, hour, everything()) %>%
     filter(year >= START_YEAR, year <= FINISH_YEAR)
 }
-East_Region_Weather<-prepare_2("data_raw/DublinAirport.csv","East",23)
-saveRDS(East_Region_Weather,"data/East_Region_Weather.rdata")
 
-West_Region_Weather<-prepare_2("data_raw/MaceHead.csv","West",)
-saveRDS(West_Region_Weather,"data/West_Region_Weather.rdata")
+setwd("C:/Users/10972490/OneDrive - National University of Ireland, Galway/Rpackages/Rpackages")
 
-Central_Region_Weather<-prepare_2("data_raw/Mullingar.csv","Central")
-saveRDS(Central_Region_Weather,"data/Central_Region_Weather.rdata")
+East_Region_Weather<-prepare_2("IrishRegionalWeatherData2024/data_raw/DublinAirport.csv","East",23)
+saveRDS(East_Region_Weather,"IrishRegionalWeatherData2024/data/East_Region_Weather.rdata")
+write_csv(East_Region_Weather,"IrishRegionalWeatherData2024/data/East_Region_Weather.csv")
 
-South_Region_Weather<-prepare_2("data_raw/Valentia.csv","South",23)
-saveRDS(South_Region_Weather,"data/South_Region_Weather.rdata")
+West_Region_Weather<-prepare_2("IrishRegionalWeatherData2024/data_raw/MaceHead.csv","West",)
+saveRDS(West_Region_Weather,"IrishRegionalWeatherData2024/data/West_Region_Weather.rdata")
+write_csv(West_Region_Weather,"IrishRegionalWeatherData2024/data/West_Region_Weather.csv")
 
-North_Region_Weather<-prepare_2("data_raw/MalinHead.csv","North",23)
-saveRDS(North_Region_Weather,"data/North_Region_Weather.rdata")
+Central_Region_Weather<-prepare_2("IrishRegionalWeatherData2024/data_raw/Mullingar.csv","Central")
+saveRDS(Central_Region_Weather,"IrishRegionalWeatherData2024/data/Central_Region_Weather.rdata")
+write_csv(Central_Region_Weather,"IrishRegionalWeatherData2024/data/Central_Region_Weather.csv")
+
+South_Region_Weather<-prepare_2("IrishRegionalWeatherData2024/data_raw/Valentia.csv","South",23)
+saveRDS(South_Region_Weather,"IrishRegionalWeatherData2024/data/South_Region_Weather.rdata")
+write_csv(South_Region_Weather,"IrishRegionalWeatherData2024/data/South_Region_Weather.csv")
+
+North_Region_Weather<-prepare_2("IrishRegionalWeatherData2024/data_raw/MalinHead.csv","North",23)
+saveRDS(North_Region_Weather,"IrishRegionalWeatherData2024/data/North_Region_Weather.rdata")
+write_csv(North_Region_Weather,"IrishRegionalWeatherData2024/data/North_Region_Weather.csv")
